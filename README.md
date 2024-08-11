0dce7a6a8027eefbdec552df94ea1b0ea2f08016
jstewart37 <23357016+jstewart37@users.noreply.github.com>
2024-08-11 20:31:41 +0100

    Set pointerevents: none on image

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # country-guessr
 Country outline guessing. worldle clone. preact/vite SPA. 
+
+### Countries SVGS
+Assets from [djaiss/mapsicon](https://github.com/djaiss/mapsicon/tree/master/all/)
--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const OPENSTREETMAP_BASEURL = "https://openstreetmap.org/#map=6/" // 49.526/5.243 // lat/long
 
-export var debug = true
+export var debug = false
 export var debugCountry = 'GB' // set to 2 char code or 'random'
 
 export const CONTACT = ''
